b0735938b4387222d19cf692588bad2c670b9ffa
Bartłomiej Tarczyński <bartlomiej.tarczynski1@gmail.com>
2020-12-15 23:17:09 +0100

    Image component

---
+++ components/AppImage.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card max-width="300">
+    <v-img
+      :src="getImageURL()"
+      lazy-src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
+      max-width="300"
+      max-height="450"
+    >
+      <template v-slot:placeholder>
+        <v-row
+          class="fill-height ma-0"
+          align="center"
+          justify="center"
+        >
+          <v-progress-circular
+            indeterminate
+            color="grey lighten-5"
+          />
+        </v-row>
+      </template>
+    </v-img>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'AppImage',
+  props: {
+    path: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    getImageURL () {
+      return `https://image.tmdb.org/t/p/w500${this.path}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/movie/_id.vue
+++ pages/movie/_id.vue
@@ -1,19 +1,24 @@
 <template>
-  <div />
+  <section>
+    <AppImage :path="movie.poster_path" />
+  </section>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import AppImage from '@/components/AppImage'
 
 export default {
   name: 'Id',
+  components: { AppImage },
   async fetch () {
     this.movie = await this.getFromBackend(this.id)
   },
   data () {
     return {
       id: this.$route.params.id,
-      movie: []
+      movie: [],
+      imageURL: ''
     }
   },
   methods: {
