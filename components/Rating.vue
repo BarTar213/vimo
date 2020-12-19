614be23f363d067e3b5f637c5eb6224523b170f2
Bartłomiej Tarczyński <bartlomiej.tarczynski1@gmail.com>
2020-12-19 19:26:14 +0100

    Backdrop movie image

--- components/Rating.vue
+++ components/Rating.vue
@@ -1,16 +1,61 @@
 <template>
   <div>
-    <v-icon x-large>
-      mdi-star
-    </v-icon>
-    <h1>{{ rating }}</h1>
-    <v-btn
-      icon
-    >
-      <v-icon x-large @click="like">
-        {{ hearIcon }}
-      </v-icon>
-    </v-btn>
+    <v-row>
+      <v-col align-self="center" cols="2">
+        <v-progress-circular
+          :value="10*rating"
+          :width="10"
+          :size="50"
+          color="cyan lighten-3"
+        >
+          {{ rating }}
+        </v-progress-circular>
+      </v-col>
+      <v-col align-self="center" cols="2">
+        <v-menu
+          v-model="value"
+          :open-on-hover="true"
+          :offset-y="true"
+          :close-delay="500"
+        >
+          <template v-slot:activator="{on, attrs}">
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon x-large>
+                mdi-star
+              </v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-rating
+              v-model="userRating"
+              :length="5"
+              empty-icon="mdi-star-outline"
+              full-icon="mdi-star"
+              half-icon="mdi-star-half-full"
+              :half-increments="true"
+              :hover="true"
+              size="30"
+              color="cyan lighten-3"
+              background-color="grey lighten-1"
+              :close-delay="500"
+            />
+          </v-card>
+        </v-menu>
+      </v-col>
+      <v-col align-self="center" cols="2">
+        <v-btn
+          icon
+        >
+          <v-icon x-large :color="color" @click="like">
+            {{ hearIcon }}
+          </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -30,7 +75,10 @@ export default {
   },
   data () {
     return {
-      hearIcon: 'mdi-heart-outline'
+      hearIcon: 'mdi-heart-outline',
+      value: false,
+      userRating: 0,
+      color: 'pink lighten-1'
     }
   },
   watch: {
@@ -51,5 +99,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
--- pages/movie/_id.vue
+++ pages/movie/_id.vue
@@ -1,26 +1,31 @@
 <template>
-  <v-container class="d-flex justify-center mb-6">
-    <v-row align="center" style="max-width: 1220px;">
-      <v-col cols="12">
-        <v-row>
-          <v-col align-self="center" cols="3">
-            <AppImage :path="movie.poster_path" />
-          </v-col>
-          <v-col align-self="start" cols="9">
-            <h1>{{ movie.title }}</h1>
-            <div class="facts">
-              <span class="fact">{{ toDate() }}</span>
-              <span class="fact">• {{ genreArray() }}</span>
-              <span class="fact">• {{ convertTime(movie.runtime) }}</span>
-            </div>
-            <h2>Overview</h2>
-            <h5>{{ movie.overview }}</h5>
-            <Rating :liked="liked" :rating="movie.vote_average" @like="like" />
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-img
+    :src="backdropURL"
+    gradient="90deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0.6138830532212884) 50%, rgba(0,0,0,1) 100%"
+  >
+    <v-container class="d-flex justify-center mb-6">
+      <v-row align="center" style="max-width: 1220px;">
+        <v-col cols="12">
+          <v-row>
+            <v-col align-self="center" cols="3">
+              <AppImage :path="movie.poster_path" />
+            </v-col>
+            <v-col align-self="start" cols="9">
+              <h1>{{ movie.title }}</h1>
+              <div class="facts">
+                <span class="fact">{{ toDate() }}</span>
+                <span class="fact">• {{ genreArray() }}</span>
+                <span class="fact">• {{ convertTime(movie.runtime) }}</span>
+              </div>
+              <h2>Overview</h2>
+              <h5>{{ movie.overview }}</h5>
+              <Rating :liked="liked" :rating="movie.vote_average" @like="like" />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-img>
 </template>
 
 <script>
@@ -37,12 +42,14 @@ export default {
   async fetch () {
     this.movie = await this.getFromBackend(this.id)
     this.liked = await this.checkLikedBackend(this.id)
+
+    this.backdropURL = `http://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${this.movie.backdrop_path}`
   },
   data () {
     return {
       id: this.$route.params.id,
       movie: [],
-      imageURL: '',
+      backdropURL: '',
       liked: false
     }
   },
@@ -70,8 +77,13 @@ export default {
       return result + minutes + 'm'
     },
     like () {
-      this.likeMovieBackend({ id: this.movie.id, beforeVal: this.liked })
-        .then(() => { this.liked = !(this.liked) })
+      this.likeMovieBackend({
+        id: this.movie.id,
+        beforeVal: this.liked
+      })
+        .then(() => {
+          this.liked = !(this.liked)
+        })
     }
   }
 }
@@ -90,5 +102,9 @@ export default {
   align-content: center;
   padding: 0.06em 4px 0.15em 4px !important;
   line-height: 1;
+}
+
+.background-gradient {
+  background: linear-gradient(90deg, rgba(0, 0, 0, 1) 0%, rgba(255, 255, 255, 0.5046393557422969) 50%, rgba(0, 0, 0, 1) 100%);
 }
 </style>
